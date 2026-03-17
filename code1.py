523a555f774c25c48be0e9107142bf201ca95665
unknown <gdutt2304@gmail.com>
2026-03-17 16:27:30 +0530

    first commit

---
+++ code1.py
@@ -0,0 +1,33 @@
+# a = 6
+# print(a , type(a))
+# b = 5.7
+# print(b , type(b))
+
+# name = "Ganesh Dutt"
+# print(name , type(name))
+
+# is_active  =  True
+# print(is_active , type(is_active))
+# name  = input("Enter your name :  ")
+# age   =  input("Enter your age : ")
+# name  = "Aman"
+# age   =  23
+# print("Name : ", name , "Age  : ",age)
+# a  =  input("Enter your number ")
+# print(a)
+
+num  = int("10")
+print(num , type(num))
+
+age  =  str(23)
+print(age , type(age))
+
+age  =  float(23)
+print(age , type(age))
+
+"""
+print(bool(1))
+print(bool(0))
+print(bool(""))
+"""
+# ctrl + / - comment 
---
+++ code2.py
@@ -0,0 +1,54 @@
+a  =  12
+b = 8
+# print(a +  b)
+# print(a -  b)
+# print(a *  b)
+# print(a /  b)
+# print(a //  b)
+# print(a **  b)
+# print(a % b)
+
+# print(a ==  b)
+# print(a !=  b)
+# print(a >  b)
+# print(a <  b)
+# print(a <=  b)
+# print(a >=  b)
+
+# AND  , OR ,NOT
+# and
+# True |  False = False 
+# False | True  = False 
+# True | True  = True
+# False | False  = False
+
+# or 
+# True | False = True 
+# False | True  =True
+# True | True  = True
+# False | False  = False
+
+
+# not 
+# not True = False
+# not False = True
+
+
+# a = 5
+# b = 10
+# c = 20
+
+# print(a >  b and c > a)
+# print( not c > a)
+
+# a  = 6 
+# a +=6    #a =  a + 6
+# a -=6    #a =  a - 6
+# a *=6    #a =  a* 6
+# a //=6    #a =  a/ 6
+# a %=6    #a =  a % 6
+# a **=6    #a =  a ** 6
+
+# a =  2 
+# a**=2  # a = a**2
+# print(a)
