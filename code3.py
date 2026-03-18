6bb493b26ae101672992e2d1a7c568e57950de0d
unknown <gdutt2304@gmail.com>
2026-03-18 15:50:37 +0530

    questions

--- code3.py
+++ code3.py
@@ -5,13 +5,23 @@
 #     print("You have failed the exam")    
 
 
-marks = 8
+# marks = 8
 
-if marks >= 90:
-    print("Grade A")
-elif marks >= 75:
-    print("Grade B")
-elif marks >= 60:
-    print("Grade C")
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 75:
+#     print("Grade B")
+# elif marks >= 60:
+#     print("Grade C")
+# else:
+#     print("Grade F")    
+
+age  = 7
+if age < 5:
+    print("Ticket free hai")
+elif age >5 and age <= 15:
+    print("Ticket price is 100")
+elif age > 15 and age <= 60:
+    print("Ticket price is 200")
 else:
-    print("Grade F")    
+    print("Ticket price is 150")    
