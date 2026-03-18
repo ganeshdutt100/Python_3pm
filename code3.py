96cfdee68031e144fa860e3685cefd10bffad237
unknown <gdutt2304@gmail.com>
2026-03-18 15:33:00 +0530

    conditional Statement

---
+++ code3.py
@@ -0,0 +1,17 @@
+# marks = 50
+# if marks >= 50:
+#     print("You have passed the exam")
+# else:
+#     print("You have failed the exam")    
+
+
+marks = 8
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Grade F")    
