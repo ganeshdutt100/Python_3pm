2695e860c1581a7d213c212c77e962cfa8cc0a04
unknown <gdutt2304@gmail.com>
2026-03-19 15:58:45 +0530

    loop

---
+++ code5.py
@@ -0,0 +1,25 @@
+# for i in range(start, end(n-1))
+# for i in range(1,5):
+#     print(i)
+
+# for i in range(1,11):
+#     print(f"apple number {i} Wash ho gye hai")    
+
+# while condition
+# data = False
+# recharge = 0
+
+# while data  ==  False:
+#     recharge+=10
+#     print(f"Recharge done {recharge}")
+
+#     if recharge == 250:
+#         data = True
+# print("data")
+
+battery = 98
+
+while battery < 100:
+    battery +=1
+    print(f"Charging... Current battery: {battery}%")
+print("Battery Full")    
