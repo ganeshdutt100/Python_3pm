026d5e89bcd96d9ac63e8ecf1015fb123953b29b
unknown <gdutt2304@gmail.com>
2026-03-23 15:16:00 +0530

    GAME

---
+++ code6.py
@@ -0,0 +1,18 @@
+villain =  100
+player = 50
+
+print("------Battle Started---------- ")
+
+while villain > 0:
+    print(f"Villain Health : {villain}")
+    input("Press Enter to Attack 👊 ")
+
+    villain -= player 
+   
+if villain <= 0:
+    print("Booooom! You Win!")
+else:
+    print("Villain is still standing! keep hitting ")
+
+print("---  Game Over ------")    
+
